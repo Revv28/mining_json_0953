a8c09d406fbffd4d25bd4ee489dbadac5a34182b
Revanth C M <revanth.c-m@capgemini.com>
2024-12-02 15:21:14 +0000

    added search

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller,Filter,FilterOperator) => {
     "use strict";
 
     return Controller.extend("app.miningjson0953.controller.View1", {
@@ -15,7 +17,25 @@ sap.ui.define([
             oRouter.navTo('RouteView2',{
                 locationId:locId
             })
+        },
+        onFilterSearch: function (oEvent) {
+            var sQuery = oEvent.getParameter("newValue");
+            var oList = this.byId("idList");  
+            var oBinding = oList.getBinding("items");  
 
-        }
+            if (sQuery && sQuery.length > 0) {
+              
+                var oFilterId = new Filter("id", FilterOperator.Contains, sQuery);  
+                var oFilterName = new Filter("name", FilterOperator.Contains, sQuery);  
+                var oFilterCountry = new Filter("country", FilterOperator.Contains, sQuery); 
+                var oCombinedFilter = new Filter({
+                    filters: [oFilterId, oFilterName, oFilterCountry],
+                    and: false 
+                });
+                oBinding.filter(oCombinedFilter);
+            } else {
+                oBinding.filter([]);
+            }
+        },
     });
 });
